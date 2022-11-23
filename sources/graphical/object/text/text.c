0ccccb841602d927d13badc3efffee531edc2925
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2022-11-23 19:46:21 +0100

    [ADD] - Text object

---
+++ sources/graphical/object/text/text.c
@@ -0,0 +1,106 @@
+/*
+** Description:
+** Text's class definition
+**
+** Corentin COUTRET-ROZET
+** https://github.com/sheiiva/DuckHunt
+**
+*/
+
+#include "raise.h"
+
+#include "text.h"
+
+static void Text_setFont(TextClass *this, sfFont *font)
+{
+    if (!font)
+        return;
+
+    this->_font = font;
+    sfText_setFont(this->_text, font);
+}
+
+static const sfFont *Text_getFont(const TextClass *this)
+{
+    return sfText_getFont(this->_text);
+}
+
+static void Text_display(TextClass *this, sfRenderWindow *window)
+{
+    sfRenderWindow_drawText(window, this->_text, NULL);
+}
+
+static sfBool Text_mouseOnText(const TextClass *this, const sfRenderWindow *window)
+{
+    sfFloatRect rect = sfText_getGlobalBounds(this->_text);
+    sfVector2i mouse = sfMouse_getPositionRenderWindow(window);
+
+    if (mouse.x > rect.left && mouse.x < rect.left + rect.width
+        && mouse.y > rect.top && mouse.y < rect.top + rect.height)
+        return (sfTrue);
+    return (sfFalse);
+}
+
+static sfBool Text_clickOnText(const TextClass *this, const sfRenderWindow *window)
+{
+    if (Text_mouseOnText(this, window) && sfMouse_isButtonPressed(sfMouseLeft))
+        return (sfTrue);
+    return (sfFalse);
+}
+
+static void Text_ctor(TextClass *this, va_list *args)
+{
+    // Initialize internal resources
+    if (!(this->_text = sfText_create()))
+        raise("Can't create sfText");
+
+    setTextString(this, va_arg(*args, char*));
+    setTextSize(this, va_arg(*args, unsigned int));
+	setTextColor(this, va_arg(*args, sfColor));
+    setTextPosition(this, va_arg(*args, sfVector2f));
+
+    if (!(this->_font = sfFont_createFromFile(va_arg(*args, char*))))
+        raise("Can't create Sprite");
+    setTextFont(this, this->_font);
+
+    printf("Text()\n");
+}
+
+static void Text_dtor(TextClass *this)
+{
+    // Release internal resources
+    if (this->_text)
+        sfText_destroy(this->_text);
+    if (this->_font)
+        sfFont_destroy(this->_font);
+
+    printf("~Text()\n");
+}
+
+static const TextClass _description = {
+    {   /* Class struct */
+        .__size__ = sizeof(TextClass),
+        .__name__ = "Text",
+        .__ctor__ = (ctor_t)&Text_ctor,
+        .__dtor__ = (dtor_t)&Text_dtor,
+        .__str__ = NULL,
+        .__add__ = NULL,
+        .__sub__ = NULL,
+        .__mul__ = NULL,
+        .__div__ = NULL,
+        .__eq__ = NULL,
+        .__gt__ = NULL,
+        .__lt__ = NULL
+    },
+    ._text = NULL,
+    ._font = NULL,
+    /* Getter and Setter */
+    .__setFont__ = &Text_setFont,
+    .__getFont__ = &Text_getFont,
+    /* Methods definitions */
+    .__display__ = &Text_display,
+    .__mouseOnText__ = &Text_mouseOnText,
+    .__clickOnText__ = &Text_clickOnText
+};
+
+const Class *Text = (const Class *)&_description;
---
+++ sources/graphical/object/text/text.h
@@ -0,0 +1,74 @@
+/*
+** Description:
+** Scene's Text structure
+**
+** Corentin COUTRET-ROZET
+** https://github.com/sheiiva/DuckHunt
+**
+*/
+
+#ifndef TEXT_H_
+    #define TEXT_H_
+
+    #include <SFML/Window.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/System.h>
+
+    #include "object.h"
+
+    typedef struct s_TextClass {
+
+        /* Inheritance */
+        Class base;
+
+        /* Special Definition*/
+        sfText*         _text;
+        sfFont*   _font;
+
+        /* Getter and Setter */
+        void            (*__setFont__)(struct s_TextClass*, sfFont*);
+        const sfFont*   (*__getFont__)(const struct s_TextClass*);
+
+        /* Methods definitions */
+        void (*__display__)(struct s_TextClass*, sfRenderWindow*);
+        sfBool (*__mouseOnText__)(const struct s_TextClass*, const sfRenderWindow*);
+        sfBool (*__clickOnText__)(const struct s_TextClass*, const sfRenderWindow*);
+
+    } TextClass;
+
+    /* METHODS */
+    #define displayText(t, w)   ((TextClass*)t)->__display__(t, w)
+
+    /* GET/SET */
+
+    #define setTextString(t, s)     sfText_setString(((TextClass*)t)->_text, s)
+    #define getTextString(t)        sfText_getString(((TextClass*)t)->_text)
+
+    #define setTextColor(t, c)      sfText_setColor(((TextClass*)t)->_text, c)
+    #define getTextColor(t)         sfText_getColor(((TextClass*)t)->_text)
+
+    #define setTextPosition(t, p)   sfText_setPosition(((TextClass*)t)->_text, p)
+    #define getTextPosition(t)      sfText_getPosition(((TextClass*)t)->_text)
+
+    #define setTextSize(t, s)       sfText_setCharacterSize(((TextClass*)t)->_text, s)
+    #define getTextSize(t)          sfText_getCharacterSize(((TextClass*)t)->_text)
+
+    #define setTextFont(t, f)       ((TextClass*)t)->__setFont__(t, f)
+    #define getTextFont(t)          ((TextClass*)t)->__getFont__(t)
+
+    /* OTHERS */
+    #define mouseOnText(t, w)      ((TextClass*)t)->__mouseOnText__(t, w)
+    #define clickOnText(t, w)      ((TextClass*)t)->__clickOnText__(t, w)
+
+    #define ORANGE  (sfColor){255, 140, 0, 255}
+    #define YELLOW  (sfColor){255, 255, 0, 255}
+    #define PURPLE  (sfColor){128, 0, 128, 255}
+    #define RED     sfRed
+    #define GREEN   sfGreen
+    #define BLUE    sfBlue
+    #define BLACK   sfBlack
+    #define WHITE   sfWhite
+
+    extern const Class *Text;
+
+#endif /* !TEXT_H_ */
